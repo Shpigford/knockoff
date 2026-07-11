90759d526963766a2917c12cbb2f3a8b2c690de7
alex simmons <4629280+AlexSimmons@users.noreply.github.com>
2026-07-11 12:02:42 +0100

    Add Aigo, Avantree, Epomaker, Ikko, and Tikom to KO_CHINESE_MAJOR

--- data/chinese-major.js
+++ data/chinese-major.js
@@ -2,14 +2,14 @@
 // These are real companies with reputations (not trademark-squat pseudo-brands),
 // so they count as "known" by default. A setting lets users flag them anyway.
 var KO_CHINESE_MAJOR = [
-  "1More", "70mai", "Acebeam", "Amaran", "Amazfit", "Anbernic", "Anker",
-  "AnkerWork", "Anycubic", "Aputure", "Aqara", "Aventon", "Bambu Lab", "Baseus",
+  "1More", "70mai", "Acebeam", "Aigo", "Amaran", "Amazfit", "Anbernic", "Anker",
+  "AnkerWork", "Anycubic", "Aputure", "Aqara", "Aventon", "Avantree", "Bambu Lab", "Baseus",
   "Beelink", "BigTreeTech", "Bluetti", "Choetech", "Chuwi", "Convoy", "Cosori",
   "Creality", "DJI", "Dongcheng", "Dreame", "Dreame Hair", "Dreo", "EcoFlow",
-  "Ecovacs", "Edifier", "Elegoo", "Engwe", "Eufy", "EUY", "FeiyuTech", "Fenix",
+  "Ecovacs", "Edifier", "Elegoo", "Engwe", "Epomaker", "Eufy", "EUY", "FeiyuTech", "Fenix",
   "Fiido", "FiiO", "Flashforge", "GMKtec", "Godox", "Gotrax", "Govee", "Gree",
   "Haier", "Hiboy", "Hidizs", "Hisense", "Hollyland", "Holy Stone", "Honor",
-  "Hoto", "Huawei", "Hubsan", "Hychika", "ILIFE", "Imalent", "Insta360",
+  "Hoto", "Huawei", "Hubsan", "Hychika", "Ikko", "ILIFE", "Imalent", "Insta360", 
   "Jackery", "Kasa", "Kimo", "Kingroon", "KZ", "Laifen", "Lectric", "Lenovo",
   "Makeblock", "Mercusys", "Meross", "Midea", "Minisforum", "Miyoo", "Moondrop",
   "Moza", "Narwal", "Nebula", "Neewer", "Ninebot", "Nitecore", "Niu", "Olight",
@@ -17,7 +17,7 @@ var KO_CHINESE_MAJOR = [
   "Rad Power", "Realme", "Redmi", "Retroid", "Ride1Up", "Roborock", "Ruko",
   "Segway", "Shanling", "Skilhunt", "Slick Gorilla China", "SmallRig", "SMSL",
   "Sofirn", "Sonoff", "Soundcore", "Sovol", "Switchbot", "Tapo", "TCL",
-  "Teclast", "Tineco", "Topping", "Tozo", "TP-Link", "Tronsmart", "Tymo",
+  "Teclast", "Tikom", "Tineco", "Topping", "Tozo", "TP-Link", "Tronsmart", "Tymo",
   "Ugreen", "Ulanzi", "Ultrean", "Vantrue", "Vention", "Viofo", "Vivo", "Wemo",
   "Wolfbox", "WORKPRO", "Wuben", "Wurkkos", "Xiaomi", "Yeelight", "Zhiyun",
   "ZTE"
